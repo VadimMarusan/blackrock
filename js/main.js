7a1f8426d7ff7684e286487dc866441695a0abc0
vad <sfsf>
2022-06-20 22:15:47 +0300

    test2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,30 @@ $(function () {
 	})
 });
 
+$(function () {
+	$('.faq-button1').click(function () {
+		$('.faq-desc-item-hidden1').toggleClass('faq-desc-item-open')
+		$('.faq-button1').toggle()
+	})
+	$('.faq-button2').click(function () {
+		$('.faq-desc-item-hidden2').toggleClass('faq-desc-item-open')
+		$('.faq-button2').toggle()
+	})
+	$('.faq-button3').click(function () {
+		$('.faq-desc-item-hidden3').toggleClass('faq-desc-item-open')
+		$('.faq-button3').toggle()
+	})
+	$('.faq-button4').click(function () {
+		$('.faq-desc-item-hidden4').toggleClass('faq-desc-item-open')
+		$('.faq-button4').toggle()
+	})
+	$('.faq-button5').click(function () {
+		$('.faq-desc-item-hidden5').toggleClass('faq-desc-item-open')
+		$('.faq-button5').toggle()
+	})
+});
+
+
 
 
 
